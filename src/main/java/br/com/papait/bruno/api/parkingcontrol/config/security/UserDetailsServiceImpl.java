d083316d3c4f901a89898c726cb6751fab3bd0a8
brunopapait <brunohpapait@gmail.com>
2022-07-26 23:47:42 -0300

    feat: create new class config UserDetailsServiceImpl

---
+++ src/main/java/br/com/papait/bruno/api/parkingcontrol/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package br.com.papait.bruno.api.parkingcontrol.config.security;
+
+import br.com.papait.bruno.api.parkingcontrol.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	public UserDetailsServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		return userRepository.findByUsername(username)
+						.orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+	}
+}
