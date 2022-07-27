5f5c92218fdc1047c2ca35411f033b35221ce4ef
brunopapait <brunohpapait@gmail.com>
2022-07-26 23:48:50 -0300

    fix: refactor method configure where authentication in user save db

--- src/main/java/br/com/papait/bruno/api/parkingcontrol/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/papait/bruno/api/parkingcontrol/config/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.papait.bruno.api.parkingcontrol.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final UserDetailsServiceImpl userDetailsService;
+
+	public WebSecurityConfig(UserDetailsServiceImpl userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -26,10 +33,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-						.withUser("brunopapait")
-						.password(passwordEncoder().encode("123123"))
-						.roles("ADMIN");
+		auth.
+						userDetailsService(userDetailsService)
+						.passwordEncoder(this.passwordEncoder());
 	}
 
 	@Bean
