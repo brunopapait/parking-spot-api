4ca2f50018c07e20752271b85ad2c22f0942353e
brunopapait <brunohpapait@gmail.com>
2022-07-26 23:24:48 -0300

    feat: create interface UserRepository

---
+++ src/main/java/br/com/papait/bruno/api/parkingcontrol/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.papait.bruno.api.parkingcontrol.repository;
+
+import br.com.papait.bruno.api.parkingcontrol.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
