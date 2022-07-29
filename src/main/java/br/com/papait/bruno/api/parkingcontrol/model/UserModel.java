41c5462244c303b3e063190a517a31028d7bb84a
brunopapait <brunohpapait@gmail.com>
2022-07-28 23:20:46 -0300

    feat: refactor and create new relations many to many with roles

--- src/main/java/br/com/papait/bruno/api/parkingcontrol/model/UserModel.java
+++ src/main/java/br/com/papait/bruno/api/parkingcontrol/model/UserModel.java
@@ -5,7 +5,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -23,9 +25,15 @@ public class UserModel implements UserDetails, Serializable {
 	@Column(nullable = false)
 	private String password;
 
+	@ManyToMany
+	@JoinTable(name = "TB_USERS_ROLES",
+					joinColumns = @JoinColumn(name = "user_id"),
+					inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private List<RoleModel> roles = new ArrayList<>();
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		return this.roles;
 	}
 
 	@Override
